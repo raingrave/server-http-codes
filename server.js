9c87b1d1699f855bf7a1c21c6264bd0090088458
Luis Fernando <luis@appmax.com.br>
2020-12-22 10:11:10 -0300

    fix refatoração de log

--- server.js
+++ server.js
@@ -4,68 +4,68 @@ const app = express()
 const port = 3000
 
 app.post('/success', (request, response) => {
-  this.requestLog(request)
-  res.status(200)
+  requestLog(request)
+  response.status(200)
      .send('response 200')
 })
 
-app.post('/success-delay', (request, res) => {
+app.post('/success-delay', (request, response) => {
   setTimeout(() => {
-    this.requestLog(request)
-    res.status(200)
+    requestLog(request)
+    response.status(200)
        .send('response 200')
   }, 60000);
 })
 
-app.post('/created', (request, res) => {
-  this.requestLog(request)
-  res.status(201)
+app.post('/created', (request, response) => {
+  requestLog(request)
+  response.status(201)
      .send('response 201')
 })
 
-app.post('/created-delay', (request, res) => {
+app.post('/created-delay', (request, response) => {
   setTimeout(() => {
-    this.requestLog(request)
-    res.status(201)
+    requestLog(request)
+    response.status(201)
        .send('response 201')
   }, 60000);
 })
 
-app.post('/not-found', (request, res) => {
-  this.requestLog(request)
-  res.status(404)
+app.post('/not-found', (request, response) => {
+  requestLog(request)
+  response.status(404)
      .send('response 404')
 })
 
 
-app.post('/not-found-delay', (request, res) => {
+app.post('/not-found-delay', (request, response) => {
     setTimeout(() => {
-      this.requestLog(request)
-      res.status(404)
+      requestLog(request)
+      response.status(404)
          .send('response 404 delayed 60s')
     }, 60000)
 })
 
-app.post('/internal-server-error', (request, res) => {
-  this.requestLog(request)
-  res.status(500)
+app.post('/internal-server-error', (request, response) => {
+  requestLog(request)
+  response.status(500)
      .send('response 500')
 })
 
-app.post('/internal-server-error-delay', (request, res) => {
+app.post('/internal-server-error-delay', (request, response) => {
      setTimeout(() => {
-        this.requestLog(request)
-        res.status(500)
+        requestLog(request)
+        response.status(500)
        .send('response 500 delayed 60s')
     }, 60000)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (request, response) => {
+  request.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Server testing http://localhost:${port}`)
 })
 
-const requestLog = (request) => console.log(request.method, `${request.headers.host}${req.url}`)
+const requestLog = (request) => console.log(request.method, `${request.headers.host}${request.url}`)
