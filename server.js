8a32091133354e8bd6f40f795d273b29bdc55a9c
Luis Fernando <luis@appmax.com.br>
2020-12-22 10:05:13 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/success', (req, res) => {
+app.post('/success', (request, response) => {
+  this.requestLog(request)
   res.status(200)
      .send('response 200')
 })
 
-app.post('/success-delay', (req, res) => {
+app.post('/success-delay', (request, res) => {
   setTimeout(() => {
+    this.requestLog(request)
     res.status(200)
        .send('response 200')
   }, 60000);
 })
 
-app.post('/created', (req, res) => {
+app.post('/created', (request, res) => {
+  this.requestLog(request)
   res.status(201)
      .send('response 201')
 })
 
-app.post('/created-delay', (req, res) => {
+app.post('/created-delay', (request, res) => {
   setTimeout(() => {
+    this.requestLog(request)
     res.status(201)
        .send('response 201')
   }, 60000);
 })
 
-app.post('/not-found', (req, res) => {
+app.post('/not-found', (request, res) => {
+  this.requestLog(request)
   res.status(404)
      .send('response 404')
 })
 
 
-app.post('/not-found-delay', (req, res) => {
-  console.log(req.params)
-
-     setTimeout(() => {
-        res.status(404)
-       .send('response 404 delayed 60s')
+app.post('/not-found-delay', (request, res) => {
+    setTimeout(() => {
+      this.requestLog(request)
+      res.status(404)
+         .send('response 404 delayed 60s')
     }, 60000)
 })
 
-app.post('/internal-server-error', (req, res) => {
+app.post('/internal-server-error', (request, res) => {
+  this.requestLog(request)
   res.status(500)
      .send('response 500')
 })
 
-app.post('/internal-server-error-delay', (req, res) => {
+app.post('/internal-server-error-delay', (request, res) => {
      setTimeout(() => {
+        this.requestLog(request)
         res.status(500)
        .send('response 500 delayed 60s')
     }, 60000)
@@ -60,3 +67,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server testing http://localhost:${port}`)
 })
+
+const requestLog = (request) => console.log(request.method, `${request.headers.host}${req.url}`)
